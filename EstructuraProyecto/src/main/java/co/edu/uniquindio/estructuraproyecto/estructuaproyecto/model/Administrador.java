1351d88cfe1132624db0f8e272b55e4ae65c03aa
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2024-10-02 11:00:18 -0500

    adicion clases

---
+++ EstructuraProyecto/src/main/java/co/edu/uniquindio/estructuraproyecto/estructuaproyecto/model/Administrador.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.estructuraproyecto.estructuaproyecto.model;
+
+public class Administrador {
+}
---
+++ EstructuraProyecto/src/main/java/co/edu/uniquindio/estructuraproyecto/estructuaproyecto/model/Builder/ComentarioBuilder.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.estructuraproyecto.estructuaproyecto.model.Builder;
+
+public class ComentarioBuilder {
+}
